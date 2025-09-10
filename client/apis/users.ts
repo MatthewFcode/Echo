ae2436187391eed7ab8273dd2750136ea8f1e246
Abe Calder <abecalder1@gmail.com>
2025-09-11 10:00:50 +1200

    feat: use-hooks - progress (not 100% complete yet)

--- client/apis/users.ts
+++ client/apis/users.ts
@@ -3,12 +3,18 @@ import { User, UserData } from '../../models/User'
 
 const rootURL = new URL(`/api/v1`, document.baseURI)
 
+interface GetUserFunction {
+  token: string
+}
+
 // GET /api/v1/users (gets all users)
-export async function getUsers(token: string) {
+export async function getUsers({
+  token,
+}: GetUserFunction): Promise<User | null> {
   const response = await request
     .get(`${rootURL}/users`)
     .set('Authorization', `Bearer ${token}`)
-  return response.body as User[]
+  return response.body as User
 }
 
 // GET /api/v1/users/:id (get a specific user by id)
@@ -19,8 +25,16 @@ export async function getUserById(token: string, id: number) {
   return response.body as User
 }
 
+interface AddUserFunction {
+  newUser: UserData
+  token: string
+}
+
 // POST /api/v1/users (add a new user)
-export async function addUser(token: string, newUser: UserData) {
+export async function addUser({
+  newUser,
+  token,
+}: AddUserFunction): Promise<User> {
   return request
     .post(`${rootURL}/users`)
     .set('Authorization', `Bearer ${token}`)
---
+++ client/hooks/useUsers.ts
@@ -0,0 +1,56 @@
+import { MutationFunction, useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getUsers, getUserById, addUser } from '../apis/users'
+import { useAuth0 } from '@auth0/auth0-react'
+
+export function useUsers() {
+  const {user, getAccessTokenSilently } = useAuth0()
+
+  const query = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const token = await getAccessTokenSilently()
+      return getUsers({ token })
+    },
+    enabled: !!user,
+  })
+  return {
+    ...query,
+    add: useAddUser(),
+  }
+}
+
+export function useUserMutation<TData = unknown, TVariables = unknown>(
+  mutationFn: MutationFunction<TData, TVariables>,
+) {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+    },
+  })
+
+  return mutation
+}
+
+export function useAddUser() {
+  return useUserMutation(addUser)
+}
+
+
+export function useUserById(id: number) {
+  const { user, getAccessTokenSilently } = useAuth0()
+
+  const query = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const token = await getAccessTokenSilently()
+      return getUserById(token, id)
+    },
+    enabled: !!user
+  })
+  return {
+  ...query
+  }
+}
