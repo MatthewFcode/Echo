e4a9827bcb2b4dfc8c0e2eb3a015e33cc70b3540
Abe Calder <abecalder1@gmail.com>
2025-09-10 16:46:14 +1200

    finished user api-client-functions - still working on the other ones

--- client/apis/chats.ts
+++ client/apis/chats.ts
@@ -1,3 +1,8 @@
 import request from 'superagent'
 
 const rootURL = new URL(`/api/v1`, document.baseURI)
+
+// TODOS
+// GET /api/v1/chats (gets all chats)
+// GET /api/v1/chat/:id (gets a chat by id)
+
--- client/apis/messages.ts
+++ client/apis/messages.ts
@@ -1,3 +1,9 @@
 import request from 'superagent'
 
 const rootURL = new URL(`/api/v1`, document.baseURI)
+
+
+// POST /api/v1/messages (sends a message)
+export async function addMessage(token: string) {
+
+}
