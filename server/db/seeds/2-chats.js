8752d2f5fa956e523de7a44d211dc3ecfa3eb8de
Matthew Foley <matthewfoley333@gmail.com>
2025-09-16 14:55:01 +1200

    no changes made

--- server/db/seeds/2-chats.js
+++ server/db/seeds/2-chats.js
@@ -1,10 +1,11 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('chats').insert([
-    {id: 1, user_id: 2, user_id2: 1},
-  ]);
-};
+    { id: 1, user_id: 2, user_id2: 1 },
+    { id: 2, user_id: 3, user_id2: 1 },
+  ])
+}
