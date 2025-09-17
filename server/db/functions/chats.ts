d6efa3da17775c92d9080aa60abe1f42f854e3e2
Abe Calder <abecalder1@gmail.com>
2025-09-17 14:09:54 +1200

    cleaned up files and added a favion logo for tab

--- server/db/functions/chats.ts
+++ server/db/functions/chats.ts
@@ -2,31 +2,6 @@ import connection from '../connection.ts'
 
 const db = connection
 
-export async function getChat(userId: number, userId2: number) {
-  try {
-    const chat = await db('chats')
-      .join('users as u1', 'chats.user_id', 'u1.id')
-      .join('users as u2', 'chats.user_id2', 'u2.id')
-      .where('chats.user_id', userId)
-      .andWhere('chats.user_id2', userId2)
-      .select(
-        'chats.id',
-        'u1.id as u1Id',
-        'u1.user_name as u1UserName',
-        'u1.auth0id as u1Auth0Id',
-        'u1.profile_pic as u1ProfilePic',
-        'u2.id as u2Id',
-        'u2.user_name as u2UserName',
-        'u2.auth0id as u2Auth0Id',
-        'u2.profile_pic as u2ProfilePic',
-      )
-      .first()
-    return chat
-  } catch (err) {
-    console.log(err)
-  }
-}
-
 export async function getChatByChatId(id: number) {
   try {
     const result = await db('chats').where('id', id).returning('*')
--- server/routes/chats.ts
+++ server/routes/chats.ts
@@ -3,18 +3,6 @@ import * as db from '../db/functions/chats.ts'
 
 const router = Router()
 
-// Get a single chat
-router.get('/', async (req, res) => {
-  try {
-    const { userId, userId2 } = req.body
-    const result = await db.getChat(userId, userId2)
-    res.json(result)
-  } catch (err) {
-    console.log(err)
-    res.send(500).json('Internal server error')
-  }
-})
-
 // Get all chats from a specific userId
 router.get('/all/:id', async (req, res) => {
   try {
