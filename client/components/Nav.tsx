005e1b66181cbc3f4e761c7a18db52747645e861
Matthew Foley <matthewfoley333@gmail.com>
2025-10-13 18:22:49 +1300

    refactor change the sign in for the nav

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -32,7 +32,7 @@ function Nav() {
   }
 
   const handleSignOut = () => {
-    logout({ returnTo: window.location.origin })
+    logout({ logoutParams: { returnTo: window.location.origin } })
   }
 
   const handleSignIn = () => {
