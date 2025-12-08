6ebca4f2f937ef1ac69a87f9bd9056d7f0aa3c50
Matthew Foley <matthewfoley333@gmail.com>
2025-12-09 12:36:28 +1300

    add the websocket useEffect in the messages component, app componnent, the search component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,36 @@
 import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css'
 import Typing from './Typing'
+import { useEffect } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
 
 function App() {
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    const ws = new WebSocket('wss://echo.borb.nz/')
+    ws.onopen = () => {
+      console.log('Websocket connected')
+    }
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      if (data.type == 'database_change') {
+        queryClient.invalidateQueries({ queryKey: ['all-users'] })
+        queryClient.invalidateQueries({ queryKey: ['userById'] })
+        queryClient.invalidateQueries({ queryKey: ['chatsById'] })
+        queryClient.invalidateQueries({ queryKey: ['chats'] })
+        queryClient.invalidateQueries({ queryKey: ['chatById'] })
+        //queryClient.invalidateQueries({ queryKey: [''] })
+      }
+      ws.onclose = () => {
+        console.log('WebSocket closed')
+      }
+      ws.onerror = (error) => {
+        console.log('WebSocket error', error)
+      }
+      return () => ws.close()
+    }
+  }, [queryClient])
   return (
     <>
       <div className="app">
--- client/components/Search.tsx
+++ client/components/Search.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import { useUsers, useAllUsers } from '../hooks/useUsers.ts'
 import { useCreateChat } from '../hooks/useChats.ts'
+import { useQueryClient } from '@tanstack/react-query'
 
 function Search() {
   const [search, setSearch] = useState('')
@@ -9,6 +10,33 @@ function Search() {
   const addChat = useCreateChat()
 
   const navigate = useNavigate()
+
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    const ws = new WebSocket('wss://echo.borb.nz/')
+    ws.onopen = () => {
+      console.log('Websocket connected')
+    }
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      if (data.type == 'database_change') {
+        queryClient.invalidateQueries({ queryKey: ['all-users'] })
+        queryClient.invalidateQueries({ queryKey: ['userById'] })
+        queryClient.invalidateQueries({ queryKey: ['chatsById'] })
+        queryClient.invalidateQueries({ queryKey: ['chats'] })
+        queryClient.invalidateQueries({ queryKey: ['chatById'] })
+        //queryClient.invalidateQueries({ queryKey: [''] })
+      }
+      ws.onclose = () => {
+        console.log('WebSocket closed')
+      }
+      ws.onerror = (error) => {
+        console.log('WebSocket error', error)
+      }
+      return () => ws.close()
+    }
+  }, [queryClient])
 
   const {
     data: currentUser,
