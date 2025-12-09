440ac5f9daee281c793f94c0dcf5864ebee42349
Matthew Foley <matthewfoley333@gmail.com>
2025-12-09 13:04:37 +1300

    remvoe the unnesecary comments

--- client/components/Search.tsx
+++ client/components/Search.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router'
 import { useUsers, useAllUsers } from '../hooks/useUsers.ts'
 import { useCreateChat } from '../hooks/useChats.ts'
-// import { useQueryClient } from '@tanstack/react-query'
 
 function Search() {
   const [search, setSearch] = useState('')
@@ -10,33 +9,6 @@ function Search() {
   const addChat = useCreateChat()
 
   const navigate = useNavigate()
-
-  // const queryClient = useQueryClient()
-  // useEffect(() => {
-  //   const ws = new WebSocket('wss://echo.borb.nz/')
-  //   ws.onopen = () => {
-  //     console.log('Websocket connected')
-  //   }
-
-  //   ws.onmessage = (event) => {
-  //     const data = JSON.parse(event.data)
-  //     if (data.type == 'database_change') {
-  //       queryClient.invalidateQueries({ queryKey: ['all-users'] })
-  //       queryClient.invalidateQueries({ queryKey: ['userById'] })
-  //       queryClient.invalidateQueries({ queryKey: ['chatsById'] })
-  //       queryClient.invalidateQueries({ queryKey: ['chats'] })
-  //       queryClient.invalidateQueries({ queryKey: ['chatById'] })
-  //       //queryClient.invalidateQueries({ queryKey: [''] })
-  //     }
-  //     ws.onclose = () => {
-  //       console.log('WebSocket closed')
-  //     }
-  //     ws.onerror = (error) => {
-  //       console.log('WebSocket error', error)
-  //     }
-  //     return () => ws.close()
-  //   }
-  // }, [queryClient])
 
   const {
     data: currentUser,
